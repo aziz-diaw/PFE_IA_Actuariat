c864920a6680e7c6541ddf331b50ac8d73e1ce27
Diaw Abdoul Aziz <mawdabakh97@gmail.com>
2021-12-09 13:19:09 +0100

    RAjout des 2 derniers produits

--- Actuariat/product.py
+++ Actuariat/product.py
@@ -1,5 +1,6 @@
 import Actuariat.commutation_case_life as ccl
 import Actuariat.commutation_case_death as ccd
+import Table.tables as table
 
 
 # Pure Endowment
@@ -16,4 +17,26 @@ def Ax(x, i, table):
 def nAx(x, n, i, table):
     return (ccd.Mx(x, i, table) - ccd.Mx(x + n, i, table)) / ccl.Dx(x, i, table)
 
-# TODO : Endowment and Combined
+# TODO : Endowment and Combined :------------> Done
+
+ # Endowment
+def Ax_n(C, n, x, i, table):
+    return C*(ccl.Dx(x+n, i, table) + (ccd.Mx(x, i, table)-ccd.Mx(x+n, i, table)))/ccl.Dx(x, i, table)
+
+
+# Combined Endowment : ici, on va retourner le max entre les 2 tables
+def Cx_n(C1, C2, i, x, n, tableliste):
+    return (max((ccd.Mx(x, i, tableliste[0]) - ccd.Mx(x + n, i, tableliste[0])) / (
+                ccl.Nx(x, tableliste[0], i) - ccl.Nx(x + n, tableliste[0], i)) * C1 + ccl.Dx(x + n, i,
+                                                                                             tableliste[0]) / (
+                            ccl.Nx(x, tableliste[0], i) - ccl.Nx(x + n, tableliste[0], i)) * C2,
+                (ccd.Mx(x, i, tableliste[1]) - ccd.Mx(x + n, i, tableliste[0])) / (
+                            ccl.Nx(x, tableliste[1], i) - ccl.Nx(x + n, tableliste[1], i)) * C1 + ccl.Dx(x + n, i,
+                                                                                                         tableliste[
+                                                                                                             1]) / (
+                            ccl.Nx(x, tableliste[0], i) - ccl.Nx(x + n, tableliste[1], i)) * C2))
+
+
+### Test fonction python combined
+
+print(Cx_n(200, 100, 0.02, 40, 20, [table.TD_88_90, table.TV_88_90] ))
