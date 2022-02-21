c1c93c96979d00dc206747c07a1d2a88f2398383
Philippe2606 <negrephilippe@gmail.com>
2022-02-21 18:55:04 +0100

    new update

--- Actuariat/Reserves.py
+++ Actuariat/Reserves.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 import Table.tables
 
 
-def nAx_reserve(x, n,m, i, table, t, C=1):
-    return C*prod.nAx(x + t, n - t, i, table) - prod.annual_nAx(x,n,m,i,table) * la._a_xn_(x + t, n - t, i, table)
+def nAx_reserve(x, n,m, i, table, t, C,isIA = False):
+    if isIA :
+        return C*prod.nAx(x + t, n - t, i, table) - prod.annual_nAx(x,n,m,i,table,C,True) * la._a_xn_(x + t, n - t, i, table)
+    else :
+        return C*prod.nAx(x + t, n - t, i, table) - prod.annual_nAx(x,n,m,i,table,C) * la._a_xn_(x + t, n - t, i, table)
 
 
 
